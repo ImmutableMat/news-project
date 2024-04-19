ce9d8fd273cd43699f6843028dd8c0b888fd6ebf
IMMUTABLEMAT <mpiercy88@gmail.com>
2024-04-19 15:00:09 +0100

    completes task6

---
+++ controllers/comments.controller.js
@@ -0,0 +1,9 @@
+// const { fetchComments } = require("../models/comments.model");
+
+// exports.getComments = (request, response, next) => {
+//   fetchComments()
+//     .then((comments) => {
+//       response.status(200).send({ comments });
+//     })
+//     .catch(next);
+// };
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,3 +1,5 @@
+const format = require("pg-format");
+
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
@@ -20,3 +22,12 @@ exports.formatComments = (comments, idLookup) => {
     };
   });
 };
+
+// exports.checkExists = async (table, column, value) => {
+//   const queryStr = format("SELECT * FROM %I WHERE %I = $1;", table, column);
+//   const dbOutput = await db.query(queryStr, [value]);
+
+//   if (dbOutput.rows.length === 0) {
+//     return Promise.reject({ status: 404, message: "Resource Not Found" });
+//   }
+// };
